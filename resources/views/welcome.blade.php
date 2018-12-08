cac8d123ab0e6733cb6e011538d28856c88097d8
gafas <ry0suke.programming@gmail.com>
2018-12-08 12:41:48 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron mt-5">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            <div class="mt-4">
-            {!! link_to_route('signup.get','Sign up now!!!',[],['class'=>'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron mt-5">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get','Sign up now!!!',[],['class'=>'btn btn-lg btn-primary']) !!}
             </div>
         </div>
-    </div>
+    @endif
 @endsection
