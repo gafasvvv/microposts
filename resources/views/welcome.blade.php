92d977ea995c3becedc8e2704d9120ac8ad89a8e
gafas <ry0suke.programming@gmail.com>
2018-12-08 10:18:42 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <div class="center jumbotron mt-5">
         <div class="text-center">
-            <h1>Welcome to the Microposts</h1>            
+            <h1>Welcome to the Microposts</h1>
+            <div class="mt-4">
+            {!! link_to_route('signup.get','Sign up now!!!',[],['class'=>'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
     </div>
 @endsection
